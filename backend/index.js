a37caa5346fd3dc0b27d5f430ba4c6134c59ab13
Marco Di Summa <mdisumma@yahoo.com>
2022-02-05 02:38:21 +0000

    cors

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,12 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.get("/", (req, res) => res.send(`server running on port ${PORT}`));
 

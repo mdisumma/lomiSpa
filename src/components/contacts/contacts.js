b70a354684f4582a4053ab5c294aa1c05f00976c
Marco Di Summa <mdisumma@yahoo.com>
2022-02-11 17:47:22 +0000

    gallery

--- src/components/contacts/contacts.js
+++ src/components/contacts/contacts.js
@@ -4,6 +4,23 @@ function Contacts() {
 	return (
 		<section id="contacts">
 			<h2>Contacts</h2>
+			<ul id="social">
+				<li>
+					<a href="https://www.facebook.com/LomiSpaCebu" target="blank">
+						<i className="fab fa-facebook"></i>
+					</a>
+				</li>
+				<li>
+					<a href="https://www.instagram.com/lomispacebu10/" target="blank">
+						<i className="fab fa-instagram-square"></i>
+					</a>
+				</li>
+				<li>
+					<a href="mailto:lomispa_ph@yahoo.com">
+						<i className="fas fa-envelope"></i>
+					</a>
+				</li>
+			</ul>
 			<address>
 				<ul id="address">
 					<li>Phone: +63 32 238 8313</li>
@@ -12,14 +29,14 @@ function Contacts() {
 						Cebu, Philippines
 					</li>
 					<li>
-						mail: <a>lomispa_ph@yahoo.com</a>
+						mail: <p>lomispa_ph@yahoo.com</p>
 					</li>
 				</ul>
 			</address>
 			<iframe
 				src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3925.285853742749!2d123.96299031479684!3d10.318992892632904!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33a999d0133f4817%3A0x48d9dc1add55721a!5sLomi%20Spa!5e0!3m2!1sen!5suk!4v1619908081713!5m2!1sen!5suk"
-				allowfullscreen=""
 				loading="lazy"
+				title="googleMap"
 			></iframe>
 		</section>
 	);
--- src/components/gallery/gallery.js
+++ src/components/gallery/gallery.js
@@ -1,9 +1,35 @@
 import "./gallery.css";
 
+const imgList = [
+	{ href: "images/ls-1.jpg" },
+	{ href: "images/ls-3.jpg" },
+	{ href: "images/ls-4.jpg" },
+	{ href: "images/ls-5.jpg" },
+	{ href: "images/ls-6.jpg" },
+	{ href: "images/ls-7.jpg" },
+	{ href: "images/ls-8.jpg" },
+	{ href: "images/ls-9.jpg" },
+	{ href: "images/ls-10.jpg" },
+	{ href: "images/ls-11.jpg" },
+];
+
 function Gallery() {
 	return (
 		<div id="gallery">
-			<h2>gallery</h2>
+			<h2>Gallery</h2>
+			<div id="container">
+				<button id="buttonRight">
+					<i className="fas fa-arrow-alt-circle-right"></i>
+				</button>
+				<button id="buttonLeft">
+					<i className="fas fa-arrow-alt-circle-left"></i>
+				</button>
+				<div id="slide">
+					{imgList.map((link) => (
+						<img src={link.href} alt="" className="" loading="lazy" />
+					))}
+				</div>
+			</div>
 		</div>
 	);
 }

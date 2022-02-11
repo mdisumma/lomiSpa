33d81194a53ac457fcf5514fc9c52f316c414854
Marco Di Summa <mdisumma@yahoo.com>
2022-02-11 18:07:44 +0000

    min

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -3,12 +3,17 @@ import "./about.css";
 function About() {
 	return (
 		<section id="about">
-			<img src="./logo.svg" alt="company logo" loading="lazy" />
-			<h1>Lomi spa</h1>
-			<p>
-				Relax, recharge and pamper yourself in Lomi Spa with our traditional
-				Filipino Hilot combined with the Lomilomi technique from Hawaii.{" "}
-			</p>
+			<div className="lomiLogo">
+				<img src="./logo.svg" alt="company logo" loading="lazy" />
+				<h1>Lomi spa</h1>
+			</div>
+
+			<div className="lomiMessage">
+				<p>
+					Relax, recharge and pamper yourself in Lomi Spa with our traditional
+					Filipino Hilot combined with the Lomilomi technique from Hawaii.{" "}
+				</p>
+			</div>
 		</section>
 	);
 }

a96452bb7e9ab8a46935e33db9d897248968b3ea
Marco Di Summa <mdisumma@yahoo.com>
2022-03-06 16:18:12 +0000

    min

--- src/components/bookingData/bookingData.js
+++ src/components/bookingData/bookingData.js
@@ -1,30 +1,60 @@
 import React, { useState, useEffect } from "react";
-import "./bookingData.css"
+import "./bookingData.css";
 function BookingData() {
-    const [data, setData] = useState([]);
-    useEffect(() => {
+	const [data, setData] = useState([]);
+	const [dateValue, setDateValue] = useState("");
+
+	useEffect(() => {
 		fetch("http://localhost:4000/booking")
 			.then((response) => response.json())
 			.then(setData);
 	}, []);
-    console.log(data)
-return(
-    <div>
-        <h2>Booking List</h2>
-        {data.map((booking) => (
-        <ul key={booking._id} className="bookingData">
-            <li><b>Date:</b> {booking.date}</li>
-            <li><b>Name:</b> {booking.name}</li>
-            <li><b>Email:</b> {booking.email}</li>
-            <li><b>Service:</b> {booking.service}</li>
-            <li><b>Time:</b> {booking.time}</li>
-            <li><b>Duration:</b> {booking.duration}</li>
-            <li><b>Note:</b> {booking.note}</li>
-        </ul>
-        ))}
 
-    </div>
-    
-)
+	console.log(data);
+	if (dateValue !== "") {
+		return <div>{dateValue}</div>;
+	} else {
+		return (
+			<div>
+				<h2>Booking List</h2>
+				<label className="bookingLabels" htmlFor="dateValue">
+					select date
+				</label>
+				<input
+					className="bookingInputs"
+					id="dateValue"
+					name="dateValue"
+					type="date"
+					value={dateValue}
+					onChange={(e) => setDateValue(e.target.value)}
+				/>
+				{data.map((booking) => (
+					<ul key={booking._id} className="bookingData">
+						<li>
+							<b>Date:</b> {booking.date}
+						</li>
+						<li>
+							<b>Name:</b> {booking.name}
+						</li>
+						<li>
+							<b>Email:</b> {booking.email}
+						</li>
+						<li>
+							<b>Service:</b> {booking.service}
+						</li>
+						<li>
+							<b>Time:</b> {booking.time}
+						</li>
+						<li>
+							<b>Duration:</b> {booking.duration}
+						</li>
+						<li>
+							<b>Note:</b> {booking.notes}
+						</li>
+					</ul>
+				))}
+			</div>
+		);
+	}
 }
 export default BookingData;

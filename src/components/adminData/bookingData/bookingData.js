b6f675c547faee7354f2b8035d71493060a7d7ef
Marco Di Summa <mdisumma@yahoo.com>
2022-03-12 10:41:45 +0000

    filter appointment

--- src/components/adminData/bookingData/bookingData.js
+++ src/components/adminData/bookingData/bookingData.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./bookingData.css";
+
 function BookingData() {
 	const [data, setData] = useState([]);
 	const [dateValue, setDateValue] = useState("");
@@ -10,10 +11,11 @@ function BookingData() {
 			.then(setData);
 	}, []);
 
-	// console.log(data)
+	const filterAppointment = data.filter((item) => {
+		return item.date === dateValue;
+	});
+
 	if (dateValue !== "") {
-		return <div>{dateValue}</div>;
-	} else {
 		return (
 			<div>
 				<h2>Booking List</h2>
@@ -29,7 +31,58 @@ function BookingData() {
 						value={dateValue}
 						onChange={(e) => setDateValue(e.target.value)}
 					/>
-					<button>seletc</button>
+					<button
+						onClick={() => {
+							setDateValue("");
+						}}
+					>
+						clear
+					</button>
+				</div>
+
+				{filterAppointment.map((booking) => (
+					<ul key={booking._id} className="bookingData">
+						<li>
+							<b>Date:</b> {booking.date}
+						</li>
+						<li>
+							<b>Name:</b> {booking.name}
+						</li>
+						<li>
+							<b>Email:</b> {booking.email}
+						</li>
+						<li>
+							<b>Service:</b> {booking.service}
+						</li>
+						<li>
+							<b>Time:</b> {booking.time}
+						</li>
+						<li>
+							<b>Duration:</b> {booking.duration}
+						</li>
+						<li>
+							<b>Note:</b> {booking.notes}
+						</li>
+					</ul>
+				))}
+			</div>
+		);
+	} else {
+		return (
+			<div>
+				<h2>Booking List</h2>
+				<div className="bookingFilter">
+					<label className="bookingLabels" htmlFor="dateValue">
+						Select date
+					</label>
+					<input
+						className="bookingInputs"
+						id="dateValue"
+						name="dateValue"
+						type="date"
+						value={dateValue}
+						onChange={(e) => setDateValue(e.target.value)}
+					/>
 				</div>
 
 				{data.map((booking) => (
